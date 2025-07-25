228a67ceed1d335ad4e03f271cdf50be86f3ce6a
Minit <minitjain.work@gmail.com>
2025-07-25 18:59:14 +0530

    feature: update CreatePost (Feed)  and Tweet components

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Avatar from "react-avatar";
-import { AiTwotoneLike } from "react-icons/ai";
-import { FaComment } from "react-icons/fa";
-import { CiHeart } from "react-icons/ci";
-import { IoBookmarks } from "react-icons/io5";
+import { FaRegComment } from "react-icons/fa6";
+import { FcLike } from "react-icons/fc";
+import { CiBookmark } from "react-icons/ci";
 
 const Tweet = () => {
   return (
     <div>
       <div>
-        <div className="flex  p-4 border-b border-gray-300">
+        <div className="flex  p-4 border-b border-gray-200">
           <Avatar src="" name="User Name" size="40" />
-          <div className="ml-2">
+          <div className="ml-2 w-full">
             <div className="flex items-center ml-2">
               <h1 className="font-semibold">Alex</h1>
               <p className="text-gray-500 text-sm  ml-2"> @IamAlex123</p>
@@ -23,16 +22,29 @@ const Tweet = () => {
               </p>
             </div>
             <div className="flex justify-between mt-2">
-              <div>
-                {" "}
-                <CiHeart />{" "}
+              <div className="flex items-center group">
+                <div className="rounded-full cursor-pointer p-2 transition-all hover:bg-blue-100 active:scale-95">
+                  <FaRegComment
+                    size={"18px"}
+                    className="group-hover:text-blue-500 group-hover:scale-110"
+                  />
+                </div>
+                <p className="text-sm ml-1 group-hover:text-blue-500">12</p>
               </div>
-              <div>
-                <FaComment />{" "}
+              <div className="flex items-center group">
+                <div className="cursor-pointer p-2 rounded-full transition-all hover:bg-red-100 active:scale-95">
+                  <FcLike size={"20px"} className="group-hover:scale-110" />
+                </div>
+                <p className="text-sm ml-1 group-hover:text-red-500">12</p>
               </div>
-              <div>
-                {" "}
-                <IoBookmarks />
+              <div className="flex items-center group">
+                <div className="rounded-full cursor-pointer p-2 transition-all hover:bg-green-100 active:scale-95">
+                  <CiBookmark
+                    size={"20px"}
+                    className="group-hover:text-green-500 group-hover:scale-110"
+                  />
+                </div>
+                <p className="text-sm ml-1 group-hover:text-green-500">12</p>
               </div>
             </div>
           </div>
